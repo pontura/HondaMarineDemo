7f83a4eceb9551c5e83edf416b4ffec1ce9db800
Agustin Pontura <pontura@gmail.com>
2021-01-04 10:11:09 -0300

    water v1

---
+++ Assets/_Demo_Water/demo1/Scripts/RotationLoop.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationLoop : MonoBehaviour
+{
+    public float speed = 50;
+
+    void Update()
+    {
+        transform.Rotate(Vector3.right * Time.deltaTime * speed);
+    }
+}
